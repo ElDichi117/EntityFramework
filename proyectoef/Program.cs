62bdcd8ab8fed897de89de7dc45bd38f5ce46871
Luis Ricardo López Sánchez <ingricardolopezsanchez@gmail.com>
2022-09-05 11:53:01 -0500

    Se crea un metodo para poder modificar los datos de un elemneto que ya existe en la BD

--- proyectoef/Program.cs
+++ proyectoef/Program.cs
@@ -50,4 +50,24 @@ app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromB
     return Results.Ok();   
 });
 
+//FromRoute vamos a obtener desde la url el id del elemento que queremos modificar
+app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id)=>
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+    //Comprobamos si la tarea se encontro o no
+    if(tareaActual != null)
+    {
+        tareaActual.CategoriaId = tarea.CategoriaId;
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+        tareaActual.Descripcion = tarea.Descripcion;
+        //guardamos los cambios
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+
+    return Results.NotFound();   
+});
+
 app.Run();
