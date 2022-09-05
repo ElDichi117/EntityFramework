a7d3349d931d2541605541b46d06d3d7c2b216a1
Luis Ricardo López Sánchez <ingricardolopezsanchez@gmail.com>
2022-09-04 23:56:45 -0500

    Se agregaron datos iniciales a la base de datos

--- proyectoef/TareasContext.cs
+++ proyectoef/TareasContext.cs
@@ -21,14 +21,34 @@ public class TareasContext: DbContext
     //Se van a diseñar las restrcciones para el modelo de categoía usando Fluent API
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        //  creando colección para hacer un insersión de datos
+        List<Categoria> categoriasInit = new List<Categoria>();
+        categoriasInit.Add(new Categoria() { CategoriaId = Guid.Parse("fe2de405-c38e-4c90-ac52-da0540dfb4ef"), Nombre = "Actividades pendientes", Peso = 20});
+        categoriasInit.Add(new Categoria() { CategoriaId = Guid.Parse("fe2de405-c38e-4c90-ac52-da0540dfb402"), Nombre = "Actividades personales", Peso = 50});
+
+
         modelBuilder.Entity<Categoria>(categoria=>
         {
             categoria.ToTable("Categoria");
+
             categoria.HasKey(p=> p.CategoriaId);
+
             categoria.Property(p=> p.Nombre).IsRequired().HasMaxLength(150);
-            categoria.Property(p=>p.Descripcion);
+
+            categoria.Property(p=>p.Descripcion).IsRequired(false);
+
             categoria.Property(p=> p.Peso);
+
+            categoria.HasData(categoriasInit); 
         });
+
+        List<Tarea> tareasInit = new List<Tarea>();
+
+        //Cuando le asignamos el id de categoria en la tarea, debe ser el mismo qeu la categoria
+
+        tareasInit.Add(new Tarea() { TareaId = Guid.Parse("fe2de405-c38e-4c90-ac52-da0540dfb410"), CategoriaId = Guid.Parse("fe2de405-c38e-4c90-ac52-da0540dfb4ef"), PrioridadTarea = Prioridad.Media, Titulo = "Pago de servicios publicos", FechaCreacion = DateTime.Now });
+        tareasInit.Add(new Tarea() { TareaId = Guid.Parse("fe2de405-c38e-4c90-ac52-da0540dfb411"), CategoriaId = Guid.Parse("fe2de405-c38e-4c90-ac52-da0540dfb402"), PrioridadTarea = Prioridad.Baja, Titulo = "Terminar de ver pelicula en netflix", FechaCreacion = DateTime.Now });
+
 
         modelBuilder.Entity<Tarea>(tarea=>
         {
@@ -41,7 +61,7 @@ public class TareasContext: DbContext
 
             tarea.Property(p=> p.Titulo).IsRequired().HasMaxLength(200);
 
-            tarea.Property(p=> p.Descripcion);
+            tarea.Property(p=> p.Descripcion).IsRequired(false);
 
             tarea.Property(p=> p.PrioridadTarea);
 
@@ -49,8 +69,10 @@ public class TareasContext: DbContext
 
             tarea.Ignore(p=> p.Resumen);
 
-            tarea.Property(p=> p.Profesor);
-        
+            tarea.Property(p=> p.Profesor).IsRequired(false);
+
+            //Dice la colección de datos iniciales
+            tarea.HasData(tareasInit);
         });
     }
 }
